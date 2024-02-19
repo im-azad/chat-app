b262a12fe39cea6b15caef570cc598a31a21e665
im-azad <me.azad99@gmail.com>
2024-02-19 22:45:32 +0600

    react tailwind redux/toolkit RTK Query basic boilerplate stup

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,23 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore } from "@reduxjs/toolkit";
+import { apiSlice } from "../features/api/apiSlice";
+import authSliceReducer from "../features/auth/authSlice";
+import conversationSliceReducer from "../features/conversations/conversationSlice";
+import messagesSliceReducer from "../features/messages/messagesSlice";
 
 
 export const store = configureStore({
-  reducer: {
-    
-  },
+	
+	reducer: {
+		[apiSlice.reducerPath]: apiSlice.reducer,
+		auth: authSliceReducer,
+		conversations: conversationSliceReducer,
+		messages: messagesSliceReducer,
+	},
+	// production mode devTools: false
+	devTools: import.meta.env.PRO !== true,
+
+	// Adding the api middleware enables caching, invalidation, polling, and other features of `rtk-query`
+	middleware: (getDefaultMiddleware) =>
+		getDefaultMiddleware().concat(apiSlice.middleware),
 });
+
--- src/features/api/apiSlice.js
+++ src/features/api/apiSlice.js
@@ -1,8 +1,11 @@
-import {createApi, fetchBaseQuery} from "@reduxjs/toolkit/query/react";
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const apiSlice = createApi({
 	reducerPath: "api",
 	baseQuery: fetchBaseQuery({
-		baseUrl: "http://localhost:9000",
-	})
-})
+		baseUrl: import.meta.env.VITE_API_URL,
+	}),
+	tagTypes: [],
+	endpoints: (builder) => ({}),
+});
+ 
