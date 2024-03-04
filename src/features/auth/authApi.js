324bc855490bcd19a4e2f3bfefe68b3883a98d08
im-azad <me.azad99@gmail.com>
2024-03-04 23:26:07 +0600

    register api async function handle

--- src/features/auth/authApi.js
+++ src/features/auth/authApi.js
@@ -1,4 +1,5 @@
 import { apiSlice } from "../api/apiSlice";
+import { userLoggedIn } from "./authSlice";
 
 export const authApi = apiSlice.injectEndpoints({
 	endpoints: (builder) => ({
@@ -8,6 +9,34 @@ export const authApi = apiSlice.injectEndpoints({
 				method: "POST",
 				body: data,
 			}),
+			/**
+			 * A method that handles the start of a query.
+			 *
+			 * @param {type} arg - description of parameter
+			 * @param {Object} dispatch - The dispatch function
+			 * @param {Promise} queryFulfilled - The fulfilled query promise
+			 * @return {Promise} A promise that resolves when the query is handled
+			 */
+			async onQueryStarted(arg, { dispatch, queryFulfilled }) {
+				try {
+					const result = await queryFulfilled;
+					localStorage.setItem(
+						"auth",
+						JSON.stringify({
+							accessToken: result.data.accessToken,
+							user: result.data.user,
+						})
+					);
+					dispatch(
+						userLoggedIn({
+							accessToken: result.data.accessToken,
+							user: result.data.user,
+						})
+					);
+				} catch (error) {
+					console.log(error);
+				}
+			},
 		}),
 		login: builder.mutation({
 			query: (data) => ({
@@ -15,6 +44,34 @@ export const authApi = apiSlice.injectEndpoints({
 				method: "POST",
 				body: data,
 			}),
+			/**
+			 * A method that handles the start of a query.
+			 *
+			 * @param {type} arg - description of parameter
+			 * @param {Object} dispatch - The dispatch function
+			 * @param {Promise} queryFulfilled - The fulfilled query promise
+			 * @return {Promise} A promise that resolves when the query is handled
+			 */
+			async onQueryStarted(arg, { dispatch, queryFulfilled }) {
+				try {
+					const result = await queryFulfilled;
+					localStorage.setItem(
+						"auth",
+						JSON.stringify({
+							accessToken: result.data.accessToken,
+							user: result.data.user,
+						})
+					);
+					dispatch(
+						userLoggedIn({
+							accessToken: result.data.accessToken,
+							user: result.data.user,
+						})
+					);
+				} catch (error) {
+					console.log(error);
+				}
+			},
 		}),
 	}),
 });
